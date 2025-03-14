213d6d3cb9deb38e3547934682660ae8e2beeed3
3r1n6 <erindvora@gmail.com>
2025-03-14 20:53:42 +0100

    version 1.1.0 - GET Request

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,28 @@
 package org.example;
 
+import com.google.gson.*;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            // The connection to the link
+            URL url = new URL("https://htl-assistant.vercel.app/api/projects/sew5");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            // Read the JSON Data from the link
+            JsonElement jsonElement = JsonParser.parseReader(new InputStreamReader(connection.getInputStream()));
 
 
+            System.out.println(jsonElement.toString());
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
