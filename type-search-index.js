fa5381612d3196418fef0eb5fcac47949c64cd19
3r1n6 <erindvora@gmail.com>
2025-03-20 11:53:20 +0100

    version 2.3.1

--- type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"BookAnalysis"},{"p":"org.example","l":"BookAnalysisTask"},{"p":"org.example","l":"Main"}];updateSearchResults();
